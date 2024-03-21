5fe0f2c31e6599a8f068e60fdd3acaa146a5a998
Burhanuddin <buscbd@gmail.com>
2024-03-21 16:19:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Blogs from './Pages/Blogs'
+import Bookmarks from './Pages/Bookmarks'
+import Home from './Pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <Home />
+      <Blogs />
+      <Bookmarks />
+    </>
+  )
+}
+
+export default App
---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Footer from "../Components/Footer";
+import Nav from "../Components/Nav";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className="h-20"><Nav /></div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;
